dbf4dd98dfeb5156a54c4ff343e28bf05c4041d0
shuppo <christian.sunnari@gmail.com>
2017-02-08 16:18:28 +0200

    Update 2017-02-08T14:18:23.768Z

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,40 @@
 $(function(){
 
   mentoringBubbleClick();
+  setInterval(function(){articleTada()}, 4000);
+  designBGStuff();
+
 });
+
+
+function designBGStuff() {
+
+  // .design-img-link hover:
+
+  $('.design-img-link').hover(function(){
+  // find a color > apply that color to the bg
+
+  $(this).parent().parent().css('background-color',$(this).data('color'));
+
+  },function(){
+      $(this).parent().parent().css('background-color',$(this).parent().parent().data('orig-color'));
+
+
+    //off > revert the color
+
+  });
+
+
+
+
+}
+
+function articleTada(){
+  var randNum = Math.floor(Math.random() * $('.article-thumb').length) +1
+  $('.article-thumb').eq(randNum).addClass('is-emph')
+    .siblings().removeClass('is-emph');
+}
+
 
 function mentoringBubbleClick(){
 
@@ -48,18 +81,34 @@ $(window).scroll(function(){
 
   youtubeVidScroll();
   startMentoring();
+  startArticles();
 
 });
+
+
+function startArticles(){
+  var wScroll = $(window).scrollTop();
+
+  if($('section.articles').offset().top - $(window).height()/1.2 < wScroll) {
+    $('.article-thumb').each(function(i){
+      setTimeout(function(){
+        $('.article-thumb').eq(i).addClass('is-visible');
+      }, 300 * i);
+    });
+  }
+
+}
+
 
 function startMentoring(){
 
   var wScroll = $(window).scrollTop();
 
-  if($('section.mentoring').offset().top -500 < wScroll ) {
+  if($('section.mentoring').offset().top - $(window).height() / 2 < wScroll ) {
     if($(window).width() > 640) {
       $('.faces').addClass('launched');
 
-    if(!$('.face').hasClass('has-bubble-open') && !$this.hasClass('back-btn')){
+    if(!$('.face').hasClass('has-bubble-open')){
       setTimeout(function(){
       $('.face:nth-child(3)').addClass('has-bubble-open');
 
@@ -95,6 +144,14 @@ function mentoringNarrowStart(){
 
 
 
+}
+function mentoringWideStart() {
+  $('.faces').css({
+    'top': '0px',
+    'left': '0px'
+  });
+  $('.face:nth-child(3)').addClass('has-bubble-open')
+    .siblings().removeClass('has-bubble-open');
 }
 
 $(window).resize(function(){
